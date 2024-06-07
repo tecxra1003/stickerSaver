7297f1315ab628aa3fb8a5d475d3a73e730d2caa
Muhammad Ammaad Tehseen <tecxra1003@gmail.com>
2024-06-07 17:48:15 +0500

    Update Signin.js

--- src/app/components/Signin.js
+++ src/app/components/Signin.js
@@ -40,7 +40,6 @@ export default function Signin() {
         password,
         redirect: false,
       })
-      // router.replace("dashboard")
       if (res.error) {
 
         setError("invalid credentials")
